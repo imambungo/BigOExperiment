046bfe14330945f22892a21598e569fcffea2c9c
M. Imam Pratama <imambungo@gmail.com>
2018-08-30 17:23:37 +0700

    much better

--- BigO.java
+++ BigO.java
@@ -1,65 +1,18 @@
-import java.util.Random;
 class BigO{
     public static void main(String[] args) {
-        Random rand = new Random();
         HitungWaktu a = new HitungWaktu();
-        int dimensi;
 
         // Hitung kecepatan matrix
-        dimensi = 1000;
-        System.out.println("\nmatrix " + dimensi + "x" + dimensi);
-        MyMatrix mA = new MyMatrix(dimensi,dimensi);
-        a.startTime();
-        mA.randomMatrix();
-        a.endTime();
-        a.runTime();
-
-        dimensi += 1000;
-        System.out.println("\nmatrix " + dimensi + "x" + dimensi);
-        MyMatrix mB = new MyMatrix(dimensi, dimensi);
-        a.startTime();
-        mB.randomMatrix();
-        a.endTime();
-        a.runTime();
-
-        dimensi += 1000;
-        System.out.println("\nmatrix " + dimensi + "x" + dimensi);
-        MyMatrix mC = new MyMatrix(dimensi, dimensi);
-        a.startTime();
-        mC.randomMatrix();
-        a.endTime();
-        a.runTime();
-
-        dimensi += 1000;
-        System.out.println("\nmatrix " + dimensi + "x" + dimensi);
-        MyMatrix mD = new MyMatrix(dimensi, dimensi);
-        a.startTime();
-        mD.randomMatrix();
-        a.endTime();
-        a.runTime();
-
-        dimensi += 1000;
-        System.out.println("\nmatrix " + dimensi + "x" + dimensi);
-        MyMatrix mE = new MyMatrix(dimensi, dimensi);
-        a.startTime();
-        mE.randomMatrix();
-        a.endTime();
-        a.runTime();
-
-        dimensi += 1000;
-        System.out.println("\nmatrix " + dimensi + "x" + dimensi);
-        MyMatrix mF = new MyMatrix(dimensi, dimensi);
-        a.startTime();
-        mF.randomMatrix();
-        a.endTime();
-        a.runTime();
-
-        dimensi += 1000;
-        System.out.println("\nmatrix " + dimensi + "x" + dimensi);
-        MyMatrix mG = new MyMatrix(dimensi, dimensi);
-        a.startTime();
-        mG.randomMatrix();
-        a.endTime();
-        a.runTime();
+        int sekianKali = 7;
+        int barisKolom = 1000;
+        for (int i = 0; i < sekianKali; i++) {
+            System.out.println("\nmatrix " + barisKolom + "x" + barisKolom);
+            MyMatrix mA = new MyMatrix(barisKolom, barisKolom);
+            a.startTime();
+            mA.randomMatrix();
+            a.endTime();
+            a.runTime();
+            barisKolom += 1000;
+        }
     }
 }
